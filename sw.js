50142463a2d34765892879f0581788c3f2212aa6
Cowork <andy@thinkresilient.com>
2026-08-11 22:09:48 +0100

    Initial publish

---
+++ sw.js
@@ -0,0 +1,29 @@
+const CACHE = "ronald-training-v1";
+const CORE = ["./", "./index.html", "./data.json", "./manifest.json",
+  "./icon-180.png", "./icon-192.png", "./icon-512.png", "./icon-512-maskable.png"];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(CORE)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k))))
+  );
+  self.clients.claim();
+});
+
+// Network-first: online always gets the latest publish; offline falls back to the cache.
+self.addEventListener("fetch", (e) => {
+  if (e.request.method !== "GET") return;
+  e.respondWith(
+    fetch(e.request)
+      .then((res) => {
+        const copy = res.clone();
+        caches.open(CACHE).then((c) => c.put(e.request, copy));
+        return res;
+      })
+      .catch(() => caches.match(e.request))
+  );
+});
